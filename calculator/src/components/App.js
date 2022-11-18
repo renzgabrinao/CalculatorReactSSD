19017b67a4481e02031dc1d1f659a998a5bd77b1
Mason Barry <mbarry22@my.bcit.ca>
2022-11-18 11:25:37 -0800

    calc

---
+++ calculator/src/components/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './App.css';
+import KeyPad from './KeyPad'
+import Result from './Result'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          test
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
