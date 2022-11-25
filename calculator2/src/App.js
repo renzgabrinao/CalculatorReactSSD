db11f4dfe7022c17a19401c3ad28584d1e0e5725
renzgabrinao <50391561+renzgabrinao@users.noreply.github.com>
2022-11-25 03:26:21 -0800

    calc

--- calculator2/src/App.js
+++ calculator2/src/App.js
@@ -7,18 +7,57 @@ import { calculatorButtons } from "./components/calculator-base-button-data";
 
 function App() {
 
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("0")
+  const [operator, setOperator] = useState("")
+  const [result, setResult] = useState("")
+
+  const OPERATORS = ["+", "-", "*", "/"]
 
   useEffect(() => {
-    console.log(input)
-  }, [input])
+    console.log(`Input: ${input}\nOperator: ${operator}\nResult: ${result}`)
+  }, [input, operator, result])
 
-  const handleClick = (value) => {
-    setInput(() => {
-      return value
-    })
-    // console.log(input)
+  const handleClick = (button) => {
+    console.log(button.type)
+    
+    if(button.type === "number"){
+      if(input === "0"){
+        setInput(button.value)
+      }else{
+        setInput((prev) => `${prev}` + button.value)
+      }
+    }
+
+    if(button.type === "operator" && !OPERATORS.includes(input.at(-1))){
+      setInput((prev) => `${prev}` + button.value)
+    }
+
+    if(button.type === "enter"){
+      let result;
+      if(OPERATORS.includes(input.at(-1))){
+        let newInput = input.slice(0,-1)
+        result = Function("return " + newInput)
+      }else{
+        result = Function("return " + input)
+      }
+      setInput(result)
+      setResult(result)
+    }
+
+    if(button.type === "clear"){
+      let currentInput = input
+      if(button.text === "C") {
+        currentInput = currentInput.slice(0,-1);
+        setInput(currentInput)
+      }
+      if(button.text === "AC"){
+        setInput("0")
+        setOperator("")
+        setResult("")
+      }
+    }
   }
+
 
   return (
     <div className="App">
@@ -35,7 +74,7 @@ function App() {
                   value={x.value}
                   text={x.text}
                   type={x.type}
-                  onClick={() => {handleClick(x.value)}}
+                  onClick={() => {handleClick(x)}}
                 />
               )
             })
